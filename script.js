45ddcbeebfc2d40d17f93524150b31bc76f06e99
Dragesia <brkgzlsy@gmail.com>
2022-07-04 01:47:05 +0300

    added random color funcion ,

--- script.js
+++ script.js
@@ -77,3 +77,13 @@ clear.addEventListener("click", () => {
         ch.style.backgroundColor = "white";
     });
 });
+
+function generateRandomColor(){
+    let maxVal = 0xFFFFFF; // 16777215
+    let randomNumber = Math.random() * maxVal; 
+    randomNumber = Math.floor(randomNumber);
+    randomNumber = randomNumber.toString(16);
+    let randColor = randomNumber.padStart(6, 0);   
+    return `#${randColor.toUpperCase()}`
+}
+
