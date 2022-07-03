b57cb5405dc2911441c65ce9cd64054a0be52b96
Dragesia <brkgzlsy@gmail.com>
2022-07-04 00:34:35 +0300

    deleted everything and added functionality size buttons

--- script.js
+++ script.js
@@ -9,43 +9,18 @@ const grayer = document.querySelector("#grayer");
 
 let divs = 1;
 
-const div = document.createElement("div");
-div.classList.add("divClass");
-container.appendChild(div);
-
-let currentColor = "black";
-colorPicker.addEventListener("mouseout", () => {
-    currentColor = colorPicker.value;
-})
-
-div.addEventListener("mouseover", () => {
-    div.style.backgroundColor = currentColor;
-});
-
 upButton.addEventListener("click", () => {
-    if(divs == 100) {
+    if (divs == 50) {
         return;
-    }
-    while (container.hasChildNodes()) {
-        container.removeChild(container.firstChild);
     }
     divs++;
     size.textContent = divs;
-    for(let i=0; i<divs*divs; i++) {
-        container.appendChild(div.cloneNode(true));
-    }
 });
 
 downButton.addEventListener("click", () => {
-    if(divs == 1) {
+    if (divs == 1) {
         return;
-    }
-    while (container.hasChildNodes()) {
-        container.removeChild(container.firstChild);
     }
     divs--;
     size.textContent = divs;
-    for(let i=0; i<divs*divs; i++) {
-        container.appendChild(div.cloneNode(true));
-    }
 });
